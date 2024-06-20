ad4b2d17f1bce360204a4e643b7fbc4a48db6cde
Shinjan-saha <shinjansaha02@gmail.com>
2024-06-20 19:07:04 +0530

    4th Sem Done

--- ece/lib/semester_4_page.dart
+++ ece/lib/semester_4_page.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Semester4Page extends StatelessWidget {
+class Semester4Page extends StatefulWidget {
+  @override
+  _Semester4PageState createState() => _Semester4PageState();
+}
+
+class _Semester4PageState extends State<Semester4Page> {
+  String _selectedOption = 'Syllabus';
+
+  final Map<String, List<String>> _imageGroups = {
+    'Syllabus': ['4thsem.png'],
+    'Analog Communication': ['4thsem1.png', '4thsem2.png'],
+    'Analog Circuit': ['4thsem3.png'],
+    'Microprocessor & Microcontroller': ['4thsem4.png'],
+    'Design & Analysis of Algorithm': ['4thsem5.png', '4thsem6.png'],
+    'Numerical methods(BS)': ['4thsem7.png'],
+    'Biology for Engineers': ['4thsem8.png', '4thsem9.png', '4thsem10.png'],
+    'Analog Commuinctation Lab': ['4thsem11.png'],
+    'Analog Circuit Lab': ['4thsem12.png'],
+    'MicroProccessor & MicroController Lab': ['4thsem13.png'],
+    'Numerical Method Lab': ['4thsem14.png'],
+    'Soft Skill Development Lab': ['4thsem15.png'],
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,133 +33,29 @@ class Semester4Page extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Text('Official Syllabus from Makaut'),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 16, // Display 12 images
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return Image.asset(
-                    'asset/4thsem.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                } else if (index == 1) {
-                  return Image.asset(
-                    'asset/4thsem1.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 2) {
-                  return Image.asset(
-                    'asset/4thsem2.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 3) {
-                  return Image.asset(
-                    'asset/4thsem3.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 4) {
-                  return Image.asset(
-                    'asset/4thsem4.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 5) {
-                  return Image.asset(
-                    'asset/4thsem5.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 6) {
-                  return Image.asset(
-                    'asset/4thsem6.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 7) {
-                  return Image.asset(
-                    'asset/4thsem7.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 8) {
-                  return Image.asset(
-                    'asset/4thsem8.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 9) {
-                  return Image.asset(
-                    'asset/4thsem10.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 10) {
-                  return Image.asset(
-                    'asset/4thsem11.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 11) {
-                  return Image.asset(
-                    'asset/4thsem12.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                 else if (index == 12) {
-                  return Image.asset(
-                    'asset/4thsem13.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                 else if (index == 13) {
-                  return Image.asset(
-                    'asset/4thsem14.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                 else if (index == 14) {
-                  return Image.asset(
-                    'asset/4thsem15.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                
-                return Container(); // Placeholder for other indices
+            DropdownButton<String>(
+              value: _selectedOption,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedOption = newValue!;
+                });
               },
+              items: _imageGroups.keys.map<DropdownMenuItem<String>>((String key) {
+                return DropdownMenuItem<String>(
+                  value: key,
+                  child: Text(key),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: _imageGroups[_selectedOption]!
+                  .map((imageName) => Image.asset(
+                        'asset/$imageName',
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ))
+                  .toList(),
             ),
           ],
         ),
@@ -145,3 +63,5 @@ class Semester4Page extends StatelessWidget {
     );
   }
 }
+
+
