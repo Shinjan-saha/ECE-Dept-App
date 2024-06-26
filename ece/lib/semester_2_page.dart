819c4580aba1df8ee362c0bb718e6d57b5ce9a76
Shinjan-saha <shinjansaha02@gmail.com>
2024-06-26 11:17:57 +0530

    2nd Semester Dome

--- ece/lib/semester_2_page.dart
+++ ece/lib/semester_2_page.dart
@@ -1,127 +1,115 @@
 import 'package:flutter/material.dart';
 
-class Semester2Page extends StatelessWidget {
+class Semester2Page extends StatefulWidget {
+  @override
+  _Semester2PageState createState() => _Semester2PageState();
+}
+
+class _Semester2PageState extends State<Semester2Page> {
+  String _selectedOption = '2nd Sem Syllabus';
+
+  final Map<String, List<String>> _imageGroups = {
+    '2nd Sem Syllabus': ['2ndsem.png'],
+    'Physics-1': ['2ndsem1.png','2ndsem2.png'],
+    'Mathematics-2 B': ['2ndsem3.png','2ndsem4.png'],
+    'Programming for Problem Solving': ['2ndsem5.png','2ndsem6.png'],
+    'Programming for Problem Solving Lab': ['2ndsem7.png'],
+    'English': ['2ndsem8.png','2ndsem9.png','2ndsem10.png','2ndsem11.png'],
+    'Language Labratory': ['2ndsem12.png'],
+    'Physics Labratory': ['2ndsem13.png'],
+  
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('2nd Semester Syllabus'),
+        backgroundColor: Colors.teal,
       ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Text('Official Syllabus from Makaut'),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 14, // Display 12 images
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return Image.asset(
-                    'asset/2ndsem.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                } else if (index == 1) {
-                  return Image.asset(
-                    'asset/2ndsem1.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 2) {
-                  return Image.asset(
-                    'asset/2ndsem2.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 3) {
-                  return Image.asset(
-                    'asset/2ndsem3.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 4) {
-                  return Image.asset(
-                    'asset/2ndsem4.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 5) {
-                  return Image.asset(
-                    'asset/2ndsem5.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 6) {
-                  return Image.asset(
-                    'asset/2ndsem6.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 7) {
-                  return Image.asset(
-                    'asset/2ndsem7.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 8) {
-                  return Image.asset(
-                    'asset/2ndsem8.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 9) {
-                  return Image.asset(
-                    'asset/2ndsem10.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 10) {
-                  return Image.asset(
-                    'asset/2ndsem11.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                else if (index == 11) {
-                  return Image.asset(
-                    'asset/2ndsem12.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                 else if (index == 12) {
-                  return Image.asset(
-                    'asset/2ndsem13.png',
-                    // height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  );
-                }
-                
-                return Container(); // Placeholder for other indices
-              },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Official Syllabus from Makaut',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.teal[50],
+                  borderRadius: BorderRadius.circular(12), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: _selectedOption,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedOption = newValue!;
+                        });
+                      },
+                      dropdownColor: Colors.teal[50],
+                      items: _imageGroups.keys
+                          .map<DropdownMenuItem<String>>((String key) {
+                        return DropdownMenuItem<String>(
+                          value: key,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              key,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      icon: Icon(Icons.arrow_drop_down, color: Colors.teal),
+                      style: TextStyle(color: Colors.teal, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: _imageGroups[_selectedOption]!
+                  .map((imageName) => Container(
+                        margin: EdgeInsets.symmetric(vertical: 10.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[50], 
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), 
+                            ),
+                          ],
+                        ),
+                        child: InteractiveViewer(
+                          panEnabled: true,
+                          minScale: 0.5,
+                          maxScale: 4.0,
+                          child: Image.asset(
+                            'asset/$imageName',
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ))
+                  .toList(),
             ),
           ],
         ),
@@ -129,3 +117,5 @@ class Semester2Page extends StatelessWidget {
     );
   }
 }
+
+
